dd867aa490b3dfe757c6f10b525e0f45b649be93
Mohamed Moustafa <92525570+Dev-MohamedMoustafa@users.noreply.github.com>
2021-10-27 13:28:58 +0200

    Add files via upload
    initial commit

---
+++ DogAbstractFactory.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * this code was written by Mohamed Moustafa
+ * As a preview of his knowledge in Design Patterns
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class DogAbstractFactory : IAnimalAbstractFactory
+    {
+        public IAnimal CreateAnimal()
+        {
+            return new Dog();
+        }
+    }
+}
